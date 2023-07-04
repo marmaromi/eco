21b7f377936f646bdc08ab262ed58b23be217a5e
Ido Marom <marmaromi@gmail.com>
2023-07-04 22:23:13 +0300

    # nest - Added JWT and guards

--- apartment-protection/apps/apartment-protection-nest-backend/src/app/auth/strategy/jwt.strategy.ts
+++ apartment-protection/apps/apartment-protection-nest-backend/src/app/auth/strategy/jwt.strategy.ts
@@ -11,7 +11,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
-    // this.config = config;
   }
 
   async validate(payload: { sub: string; email: string }) {
